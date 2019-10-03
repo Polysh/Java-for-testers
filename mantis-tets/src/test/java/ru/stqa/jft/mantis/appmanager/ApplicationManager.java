94d9096d74a5bede91d95224206ca9be3a829dab
Polina <sedova-pn@ural.rt.ru>
2019-10-03 17:32:51 +0500

    Задание 17 Добавлен mantis

---
+++ mantis-tets/src/test/java/ru/stqa/jft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,54 @@
+package ru.stqa.jft.mantis.appmanager;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    public static WebDriver wd;
+    public final Properties properties;
+
+
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
+
+    }
+
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseUrl"));
+    }
+
+    public static Object executeJS(String query) {
+        return ((JavascriptExecutor) (wd)).executeScript(query);
+    }
+
+    public static WebElement jQuery(String selector) {
+        return (WebElement) executeJS("return jQuery.find(\"" + selector + "\")[0]");
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+
+}
---
+++ mantis-tets/src/test/java/ru/stqa/jft/mantis/tests/TestBase.java
@@ -0,0 +1,29 @@
+package ru.stqa.jft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.jft.mantis.appmanager.ApplicationManager;
+
+import java.io.IOException;
+
+public class TestBase {
+
+
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    public TestBase() {
+    }
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws IOException {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+
+}
