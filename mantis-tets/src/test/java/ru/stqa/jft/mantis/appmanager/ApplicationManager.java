c4124f71ea38fdb40e6d7ee617295f05c3da736c
Polina <sedova-pn@ural.rt.ru>
2019-10-07 17:02:50 +0500

    ошибка login в  mantis

--- mantis-tets/src/test/java/ru/stqa/jft/mantis/appmanager/ApplicationManager.java
+++ mantis-tets/src/test/java/ru/stqa/jft/mantis/appmanager/ApplicationManager.java
@@ -12,13 +12,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.MatchResult;
 
 public class ApplicationManager {
-    public static WebDriver wd;
-    public final Properties properties;
+    private static WebDriver wd;
+    private final Properties properties;
 
 
     private String browser;
+    private RegistrationHelper registrationHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -30,14 +32,8 @@ public class ApplicationManager {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
-        if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
-        }
-        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        wd.get(properties.getProperty("web.baseUrl"));
     }
+
 
     public static Object executeJS(String query) {
         return ((JavascriptExecutor) (wd)).executeScript(query);
@@ -48,7 +44,37 @@ public class ApplicationManager {
     }
 
     public void stop() {
-        wd.quit();
+        if (wd != null) {
+            wd.quit();
+        }
     }
 
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+
+    }
+
+    public RegistrationHelper registration() {
+        if(registrationHelper ==null){
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null) {
+            if (browser.equals(BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+            }
+            wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
+    }
 }
